c9adc6bbbce2dcb313b925489e05c4213b09dccd
Булат Музафаров <bulatmuzafarov@Bulats-MacBook-Air.local>
2025-02-04 20:09:36 +0300

    j

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,9 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "myapp" / "static",
-]
+STATICFILES_DIRS = [BASE_DIR / "myapp/static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
