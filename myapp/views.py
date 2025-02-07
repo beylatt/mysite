1dc01826f5d281901f861b736f1ace4a2d670bc5
Булат Музафаров <bulatmuzafarov@Bulats-MacBook-Air.local>
2025-02-07 13:53:24 +0300

    hhh

--- myapp/views.py
+++ myapp/views.py
@@ -24,4 +24,4 @@ from django.utils.timezone import now
 from django.shortcuts import render
 
 def my_view(request):
-    return render(request, 'index.html', {'timestamp': int(now().timestamp())})
+    return render(request, 'myapp/home.html', {'timestamp': int(now().timestamp())})
